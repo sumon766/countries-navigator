979ad4fca5e6a544c10880c404da751743561e89
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-05-13 16:58:40 +0600

    Fix linters errors

--- src/components/Country.js
+++ src/components/Country.js
@@ -4,7 +4,9 @@ import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-const Country = ({ name, population, countryCode, flag }) => (
+const Country = ({
+  name, population, countryCode, flag,
+}) => (
   <div className="country">
     <h3>{name}</h3>
     <img src={flag} alt={name} />
@@ -20,6 +22,7 @@ Country.propTypes = {
   name: PropTypes.string.isRequired,
   population: PropTypes.number.isRequired,
   countryCode: PropTypes.string.isRequired,
+  flag: PropTypes.string.isRequired,
 };
 
 export default Country;
